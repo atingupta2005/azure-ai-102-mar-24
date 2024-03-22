d937adb9aaf137be1ef575609d051b1b8ef4ded7
Atin Gupta <atingupta@email.com>
2024-03-22 13:02:13 +0530

    -

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AI-102
+
+```
+cd ~
+rm -rf azure-ai-102-mar-24
+git clone https://github.com/atingupta2005/azure-ai-102-mar-24
+cd ~/azure-ai-102-mar-24
+ls
+```
--- m01/README.md.sh
+++ m01/README.md.sh
@@ -1,5 +1,8 @@
+cd ~
+rm -rf ~/azure-ai-102-mar-24
+git clone https://github.com/atingupta2005/azure-ai-102-mar-24
 sudo chmod -R a+rw /pyenv
-cd ~/ai-102/mslearn-ai-services/Labfiles/01-use-azure-ai-services/Python/rest-client/
+cd ~/azure-ai-102-mar-24/mslearn-ai-services/Labfiles/01-use-azure-ai-services/Python/rest-client/
 
 pip install python-dotenv
 
@@ -8,7 +11,7 @@ sed -i 's/mykeyneedtoreplace/e53b50cc600e4a53ade0ba1c07250030/g' .env
 python rest-client.py
 
 
-cd ~/ai-102/mslearn-ai-services/Labfiles/01-use-azure-ai-services/Python/sdk-client
+cd ~/azure-ai-102-mar-24/mslearn-ai-services/Labfiles/01-use-azure-ai-services/Python/sdk-client
 pip uninstall azure-ai-textanalytics
 pip install azure-ai-textanalytics==5.3.0
 sed -i 's/mykeyneedtoreplace/e53b50cc600e4a53ade0ba1c07250030/g' .env
